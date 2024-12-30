2b995079c12f767f81a666c675643e0b8bc85a12
Harsh Singh <sengarharshsinghrajput@gmail.com>
2024-12-30 17:24:21 +0530

    chat bot added

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -1,20 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-
-const Button = styled.button`
-    display: none;
-    width: 100%;
-    padding: 10px;
-    background-color: ${({ theme }) => theme.white};
-    color: ${({ theme }) => theme.text_black};
-    font-size: 14px;
-    font-weight: 700;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: all 0.8s ease-in-out;
-`
+// Styled components definitions
 const Card = styled.div`
     width: 330px;
     height: 490px;
@@ -28,15 +15,13 @@ const Card = styled.div`
     flex-direction: column;
     gap: 14px;
     transition: all 0.5s ease-in-out;
+
     &:hover {
         transform: translateY(-10px);
         box-shadow: 0 0 50px 4px rgba(0,0,0,0.6);
         filter: brightness(1.1);
     }
-    &:hover ${Button} {
-        display: block;
-    }
-`
+`;
 
 const Image = styled.img`
     width: 100%;
@@ -44,7 +29,7 @@ const Image = styled.img`
     background-color: ${({ theme }) => theme.white};
     border-radius: 10px;
     box-shadow: 0 0 16px 2px rgba(0,0,0,0.3);
-`
+`;
 
 const Tags = styled.div`
     width: 100%;
@@ -53,7 +38,7 @@ const Tags = styled.div`
     flex-wrap: wrap;
     gap: 8px;
     margin-top: 4px;
-`
+`;
 
 const Tag = styled.span`
     font-size: 12px;
@@ -62,7 +47,7 @@ const Tag = styled.span`
     background-color: ${({ theme }) => theme.primary + 15};
     padding: 2px 8px;
     border-radius: 10px;
-`
+`;
 
 const Details = styled.div`
     width: 100%;
@@ -70,7 +55,8 @@ const Details = styled.div`
     flex-direction: column;
     gap: 0px;
     padding: 0px 2px;
-`
+`;
+
 const Title = styled.div`
     font-size: 20px;
     font-weight: 600;
@@ -82,7 +68,7 @@ const Title = styled.div`
     -webkit-box-orient: vertical;
     overflow: hidden;
     text-overflow: ellipsis;
-`
+`;
 
 const Date = styled.div`
     font-size: 12px;
@@ -92,8 +78,7 @@ const Date = styled.div`
     @media only screen and (max-width: 768px){
         font-size: 10px;
     }
-`
-
+`;
 
 const Description = styled.div`
     font-weight: 400;
@@ -105,13 +90,14 @@ const Description = styled.div`
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     text-overflow: ellipsis;
-`
+`;
 
 const Members = styled.div`
     display: flex;
     align-items: center;
     padding-left: 10px;
-`
+`;
+
 const Avatar = styled.img`
     width: 38px;
     height: 38px;
@@ -120,15 +106,15 @@ const Avatar = styled.img`
     background-color: ${({ theme }) => theme.white};
     box-shadow: 0 0 10px rgba(0,0,0,0.2);
     border: 3px solid ${({ theme }) => theme.card};
-`
+`;
 
-const ProjectCards = ({project,setOpenModal}) => {
+const ProjectCards = ({ project, setOpenModal }) => {
     return (
-        <Card onClick={() => setOpenModal({state: true, project: project})}>
-            <Image src={project.image}/>
+        <Card onClick={() => setOpenModal({ state: true, project: project })}>
+            <Image src={project.image} />
             <Tags>
                 {project.tags?.map((tag, index) => (
-                <Tag>{tag}</Tag>
+                    <Tag key={index}>{tag}</Tag>
                 ))}
             </Tags>
             <Details>
@@ -137,13 +123,13 @@ const ProjectCards = ({project,setOpenModal}) => {
                 <Description>{project.description}</Description>
             </Details>
             <Members>
-                {project.member?.map((member) => (
-                    <Avatar src={member.img}/>
+                {project.member?.map((member, index) => (
+                    <Avatar key={index} src={member.img} />
                 ))}
             </Members>
             {/* <Button>View Project</Button> */}
         </Card>
-    )
-}
+    );
+};
 
-export default ProjectCards
+export default ProjectCards;
